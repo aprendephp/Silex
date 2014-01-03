05d4232ece605fea9df471c0f32ac08f97e71796
David Flores <dmousex@gmail.com>
2014-01-02 21:41:36 -0600

    Autoload sample

--- web/index.php
+++ web/index.php
@@ -1,6 +1,19 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+function autoload($className) {
+  $className = ltrim($className, '\\');
+  $fileName  = '';
+  $namespace = '';
+  if ($lastNsPos = strrpos($className, '\\')) {
+    $namespace = substr($className, 0, $lastNsPos);
+    $className = substr($className, $lastNsPos + 1);
+    $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+  }
+  $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+  include '../src/' . $fileName;
+}
+
+spl_autoload_register('autoload');
 
 $app = new aprendePHP\Application();
 echo $app->index();

bc32d72ee2bcf76c147cd49e195373b54b14b711
root <root@devapp.(none)>
2014-01-10 17:56:39 +0000

    application documentation

--- src/aprendePHP/Application.php
+++ src/aprendePHP/Application.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * File content aprendePHP\Application
+ */
 
 namespace aprendePHP;
 
@@ -7,34 +10,72 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Application {
 
+  /**
+   * @Symfony\Component\HttpFoundation\Response
+   */
+  private $response;
+
+  /**
+   * @Symfony\Component\HttpFoundation\Request
+   */
+  private $request;
+
+  /**
+   * @Array
+   */
+  private $paths;
+
+  /**
+   * @param $response Symfony\Component\HttpFoundation\Response
+   * @param $request Symfony\Component\HttpFoundation\Request 
+   */
   public function __construct(Response $response, Request $request){
     $this->response = $response;
     $this->request = $request;
+    $this->paths = [];
   }
 
-  function index(){
+  public function get($path, $callback){
+  	$this->paths[$path]['callback'] = $callback;
+    $this->paths[$path]['method'] = 'GET';
+  }
+
+  public function post($path, $callback){
+    $this->paths[$path]['callback'] = $callback;
+    $this->paths[$path]['method'] = 'POST';
+  }
+
+  public function delete($path, $callback){
+    $this->paths[$path]['callback'] = $callback;
+    $this->paths[$path]['method'] = 'DELETE';
+  }
+
+  public function put($path, $callback){
+    $this->paths[$path]['callback'] = $callback;
+    $this->paths[$path]['method'] = 'PUT';
+  }
+
+  public function boot(){
     $path = $this->request->getPathInfo();
-    
-    $this->get($path);
-  }
+    if (array_key_exists($path, $this->paths) && 
+      $this->paths[$path]['method'] == $this->request->getMethod()) {
+      $respuesta = call_user_func($this->paths[$path]['callback']);
 
-  public function get($path){
-
-  	switch ($path) {
-  		case '/':
-  			$this->response->setContent('Index');
-  			break;
-  		case '/contacto':
-  			$this->response->setContent('Contacto');
-  			break;
-  		default:
-  			$this->response->setContent('Pagina no encontrada.');
-  			break;
-  	}
-  	
+      if ($respuesta instanceof Response){
+        $this->response = $respuesta;
+      }
+      else{
+        $this->response->setContent($respuesta);
+      }
+    }
+    else{
+      $this->response->setStatusCode(404);
+      $this->response->setContent('Page not found');
+    }
   }
 
   public function run(){
+    $this->boot();
     $this->response->send();
   }
 
--- web/index.php
+++ web/index.php
@@ -4,10 +4,51 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Cookie;
 
 $response = new Response();
 $request = Request::createFromGlobals();
 
 $app = new aprendePHP\Application($response, $request);
-$app->index();
+
+$app->get('/', function() use($request){
+
+	if ($request->cookies->get('colaborador') == 'on')
+		return "Gracias por tu aporte";
+	else
+		return "Bienvenido";
+});
+
+$app->get('/contacto', function(){ 
+	return "contacto"; 
+});
+
+$app->get('/quienes', function(){ 
+	return new RedirectResponse('contacto'); 
+});
+
+$app->post('/enviar-email', function(){
+	return "Email enviado";
+});
+
+$app->post('/beer/add', function() use($request){
+	$name = $request->request->get('name');
+	$image = $request->files->get('image');
+	$image->move(__DIR__ . '/uploads', $image->getClientOriginalName());
+	
+	$r = new RedirectResponse('/web/index.php');
+	$r->headers->setCookie(new Cookie('colaborador','on'));
+	return $r;
+});
+
+
+$app->put('/beer/update', function(){ return "update"; });
+$app->delete('/beer/delete', function(){ return "delete"; });
+
 $app->run();
+
+
+
+
+
